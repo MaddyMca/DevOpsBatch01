d2e927a71e7da22f892415f80e4a99a7e4f5fec9
Mandar Phadke <42891031+MandarPhadke@users.noreply.github.com>
2024-06-29 12:55:30 +0530

    Delete Assignments directory

--- Assignments/Test.py
+++
@@ -1,9 +0,0 @@
-import psutil
-
-print("psutil.cpu_percent(interval=2) = %s" % (psutil.cpu_percent(interval=2),))
-
-print("psutil.cpu_percent(interval=2) = %s" % (psutil.cpu_percent(interval=2),))
-
-print("The number of CPUs is : %s" % (psutil.cpu_count(), ))
-
-print("The CPU utilization of all the CPUs is: %s" % (psutil.cpu_percent(interval=2, percpu=True), ))
